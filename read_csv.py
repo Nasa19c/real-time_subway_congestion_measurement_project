4175b7a750f7b87f6c41e7c8ba4ac25a90d4980b
ajeong <ajeong2k@gmail.com>
2023-08-26 00:09:47 +0900

    "정해진 위치에 동영상, 욜로 적용, 데이터 합"

---
+++ read_csv.py
@@ -0,0 +1,156 @@
+import csv
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib
+
+matplotlib.rcParams['font.family'] = 'AppleGothic'
+matplotlib.rcParams['axes.unicode_minus'] = False
+
+# Load data from CSV file
+data = pd.read_csv('SUBWAY_MONTH.csv', encoding='utf-8')  # 변경: 파일 경로 수정
+combined_data = data[data['역명'].isin(['서울역', '종각', '시청'])].copy()
+
+# '시청' 역의 일별 승차총승객수 그래프 그리기 함수
+def draw_si_station_chart(data):
+    # '시청' 역의 데이터만 추출
+    si_data = data[data['역명'] == '시청'].copy()  # .copy() 추가
+    # 날짜(datetime) 형식으로 변환
+    si_data['사용일자'] = pd.to_datetime(si_data['사용일자'], format='%Y%m%d')
+    plt.figure(figsize=(6, 3))
+    plt.plot(si_data['사용일자'], si_data['승차총승객수'], marker='o')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('static/si_station_chart.png')
+
+
+# '시청' 역의 요일별 승차총승객수 바 차트 그리기 함수
+def draw_si_station_weekday_chart(data):
+    # '시청' 역의 데이터만 추출
+    시청_data = data[data['역명'] == '시청'].copy()  # 복사본 생성
+    # 사용일자를 datetime 형식으로 변환
+    시청_data.loc[:, '사용일자'] = pd.to_datetime(시청_data['사용일자'], format='%Y%m%d')
+    # 사용일자에서 요일을 추출 (0: 월요일, 1: 화요일, ..., 6: 일요일)
+    시청_data.loc[:, '요일'] = 시청_data['사용일자'].dt.dayofweek
+    # 요일 이름으로 변환
+    요일별_이름 = ['월', '화', '수', '목', '금', '토', '일']
+    시청_data.loc[:, '요일명'] = 시청_data['요일'].apply(lambda x: 요일별_이름[x])
+    # 요일 순서를 월요일부터 일요일까지로 변경
+    요일별_이름_변경 = ['월', '화', '수', '목', '금', '토', '일']
+    # 요일별 승차총승객수 합계 계산 및 요일 순서대로 정렬
+    요일별_승차총승객수 = 시청_data.groupby('요일명')['승차총승객수'].sum().loc[요일별_이름_변경]
+
+    plt.figure(figsize=(6, 3))
+    요일별_승차총승객수.plot(kind='bar', color='skyblue')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plt.savefig('static/si_station_weekday_chart.png')
+
+# '종각'역의 일별승차총승객수 그래프 그리기 함수
+def draw_jong_station_chart(data):
+    # '종각' 역의 데이터만 추출
+    jong_data = data[data['역명'] == '종각'].copy()
+    # 날짜(datetime) 형식으로 변환
+    jong_data['사용일자'] = pd.to_datetime(jong_data['사용일자'], format='%Y%m%d')
+    # 날짜별 승차총승객수에 대한 꺾은선 그래프 그리기
+    plt.figure(figsize=(6, 3))
+    plt.plot(jong_data['사용일자'], jong_data['승차총승객수'], marker='o')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('static/jong_station_chart.png')
+
+# '종각' 역의 요일별 승차총승객수 바 차트 그리기 함수
+def draw_jong_station_weekday_chart(data):
+    # '종각' 역의 데이터만 추출
+    종각_data = data[data['역명'] == '종각'].copy()
+    # 사용일자를 datetime 형식으로 변환
+    종각_data.loc[:, '사용일자'] = pd.to_datetime(종각_data['사용일자'], format='%Y%m%d')
+    # 사용일자에서 요일을 추출 (0: 월요일, 1: 화요일, ..., 6: 일요일)
+    종각_data.loc[:, '요일'] = 종각_data['사용일자'].dt.dayofweek
+    # 요일 이름으로 변환
+    요일별_이름 = ['월', '화', '수', '목', '금', '토', '일']
+    종각_data.loc[:, '요일명'] = 종각_data['요일'].apply(lambda x: 요일별_이름[x])
+    # 요일 순서를 월요일부터 일요일까지로 변경
+    요일별_이름_변경 = ['월', '화', '수', '목', '금', '토', '일']
+    # 요일별 승차총승객수 합계 계산 및 요일 순서대로 정렬
+    요일별_승차총승객수 = 종각_data.groupby('요일명')['승차총승객수'].sum().loc[요일별_이름_변경]
+
+    # 요일별 막대 그래프 그리기
+    plt.figure(figsize=(10, 6))
+    요일별_승차총승객수.plot(kind='bar', color='skyblue')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plt.savefig('static/jong_station_weekday_chart.png')
+
+# '서울'역의 일별승차총승객수 그래프 그리기 함수
+def draw_se_station_chart(data):
+    # '서울' 역의 데이터만 추출
+    se_data = data[data['역명'] == '서울역'].copy()
+    # 날짜(datetime) 형식으로 변환
+    se_data['사용일자'] = pd.to_datetime(se_data['사용일자'], format='%Y%m%d')
+
+    # 날짜별 승차총승객수에 대한 꺾은선 그래프 그리기
+    plt.figure(figsize=(6, 3))
+    plt.plot(se_data['사용일자'], se_data['승차총승객수'], marker='o')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('static/se_station_chart.png')
+
+def draw_se_station_weekday_chart(data):
+    # '서울' 역의 데이터만 추출
+    서울_data = data[data['역명'] == '서울역'].copy()
+    # 사용일자를 datetime 형식으로 변환
+    서울_data.loc[:, '사용일자'] = pd.to_datetime(서울_data['사용일자'], format='%Y%m%d')
+    # 사용일자에서 요일을 추출 (0: 월요일, 1: 화요일, ..., 6: 일요일)
+    서울_data.loc[:, '요일'] = 서울_data['사용일자'].dt.dayofweek
+    # 요일 이름으로 변환
+    요일별_이름 = ['월', '화', '수', '목', '금', '토', '일']
+    서울_data.loc[:, '요일명'] = 서울_data['요일'].apply(lambda x: 요일별_이름[x])
+    # 요일 순서를 월요일부터 일요일까지로 변경
+    요일별_이름_변경 = ['월', '화', '수', '목', '금', '토', '일']
+    # 요일별 승차총승객수 합계 계산 및 요일 순서대로 정렬
+    요일별_승차총승객수 = 서울_data.groupby('요일명')['승차총승객수'].sum().loc[요일별_이름_변경]
+
+    # 요일별 막대 그래프 그리기
+    plt.figure(figsize=(6, 3))
+    요일별_승차총승객수.plot(kind='bar', color='skyblue')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plt.savefig('static/se_station_weekday_chart.png')
+
+def draw_station_chart(data):
+    combined_data['사용일자'] = pd.to_datetime(combined_data['사용일자'], format='%Y%m%d')
+
+    # 일별 승차총승객수 합계 계산
+    daily_total_passengers = combined_data.groupby('사용일자')['승차총승객수'].sum()
+
+    # 일별 꺾은선 그래프 그리기
+    plt.figure(figsize=(6, 3))
+    plt.plot(daily_total_passengers.index, daily_total_passengers.values, marker='o')
+    plt.grid(True)
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig('static/all_station_chart.png')
+
+def draw_station_weekday_chart(data):
+    combined_data.loc[:,'사용일자'] = pd.to_datetime(combined_data['사용일자'], format='%Y%m%d')
+    combined_data.loc[:,'요일'] = combined_data['사용일자'].dt.dayofweek
+
+    # 요일 이름으로 변환
+    요일별_이름 = ['월', '화', '수', '목', '금', '토', '일']
+    combined_data.loc[:,'요일명'] = combined_data['요일'].apply(lambda x: 요일별_이름[x])
+
+    # 요일별 순서 정의
+    요일별_순서 = ['월', '화', '수', '목', '금', '토', '일']
+
+    # 요일별 승차총승객수 합계 계산 및 요일 순서대로 정렬
+    요일별_승차총승객수 = combined_data.groupby('요일명')['승차총승객수'].sum().loc[요일별_순서]
+
+    # 요일별 막대 그래프 그리기
+    plt.figure(figsize=(6, 3))
+    요일별_승차총승객수.plot(kind='bar', color='skyblue')
+    plt.xticks(rotation=0)
+    plt.tight_layout()
+    plt.savefig('static/all_station_weekday_chart.png')
---
+++ testing_Web.py
@@ -0,0 +1,175 @@
+from flask import Flask, render_template, Response, redirect, request, session
+import cv2
+import numpy as np
+from ultralytics import YOLO
+import pandas as pd
+from read_csv import draw_si_station_chart, draw_si_station_weekday_chart, draw_jong_station_chart, draw_jong_station_weekday_chart, draw_se_station_chart, draw_se_station_weekday_chart, draw_station_chart, draw_station_weekday_chart
+
+
+app = Flask(__name__)
+app.secret_key = 'supersecretkey'
+cap = cv2.VideoCapture("video1.mp4")
+model = YOLO("yolov8m.pt")
+
+people_bbox = []  # 감지된 사람의 수를 저장할 리스트
+paused = False  # 동영상 일시 정지 여부를 저장하는 변수
+
+users = {
+    'bean': '1234'
+}
+topics = [
+    {'id' : '종각역', 'last': '', 'next': '시청역', 'arrive1': '18:40', 'arrive2': '18:45'},
+    {'id' : '시청역','last':'종각역', 'next': '서울역', 'arrive1': '18:47', 'arrive2': '18:53'},
+    {'id' : '서울역','last':'시청역', 'next': '', 'arrive1': '18:54', 'arrive2': '18:59'}
+]
+# Load data from CSV file
+data = pd.read_csv('SUBWAY_MONTH.csv', encoding='utf-8')  # 변경: 파일 경로 수정
+
+draw_station_chart(data)
+draw_station_weekday_chart(data)
+draw_si_station_chart(data)
+draw_si_station_weekday_chart(data)
+draw_jong_station_chart(data)
+draw_jong_station_weekday_chart(data)
+draw_se_station_chart(data)
+draw_se_station_weekday_chart(data)
+
+def gen_frames():
+    global paused, last_people  # paused 변수를 전역 변수로 선언
+
+    while True:
+        if not paused:
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            results = model(frame, device="mps")
+            result = results[0]
+            bboxes = np.array(result.boxes.xyxy.cpu(), dtype="int")
+            classes = np.array(result.boxes.cls.cpu(), dtype="int")
+            
+            persons_mask = classes == 0  # 0은 persons 클래스의 인덱스
+            frame_persons_bboxes = bboxes[persons_mask]
+            
+            num_persons = frame_persons_bboxes.shape[0]  # 현재 프레임에서 감지된 사람의 수
+            people_bbox.append(num_persons)
+            
+            last_people = 0  # 기본값을 0으로 설정
+            current_people = 0
+
+            for bbox in frame_persons_bboxes:
+                (x, y, x2, y2) = bbox
+                cv2.rectangle(frame, (x, y), (x2, y2), (0, 0, 225), 2)
+                cv2.putText(frame, "Person", (x, y-5), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 225), 2)
+            
+            ret, buffer = cv2.imencode('.jpg', frame)
+            frame = buffer.tobytes()
+            
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+        else:
+            yield (b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/toggle_pause')
+def toggle_pause(): #특정 키를 누르면 last_people에 특정 키를 누른 시점의 값을 저장하도록 하자.
+    global paused, last_people
+    paused = not paused
+    if paused:
+        last_people = people_bbox[-1] if people_bbox else 0
+        print("Last People:", last_people)
+    return "Paused" if paused else "Playing" 
+
+@app.route('/save_people_bbox')
+def save_people_bbox():
+    global people_bbox
+    current_people = people_bbox[-1] if people_bbox else 0
+    saved_people = current_people
+    print("current people: ", current_people)
+    return str(saved_people)
+
+def calculate_progress_values(current_people):
+    progress_value_platform = (current_people / 30) * 100
+    progress_value_inside = (current_people / 30) * 100
+    return progress_value_platform, progress_value_inside
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if username in users and users[username] == password:
+            session['username'] = username
+            return redirect('/index2')
+        else:
+            return render_template('404.html', error='Invalid username or password')
+    else:
+        return render_template('login.html')
+
+@app.route("/index2", methods=['GET', 'POST'])
+def index2():
+    progress_value_platform = 0
+    progress_value_inside = 0    # 초기 값으로 설정
+
+    if request.method == 'POST':
+        for station_info in topics:
+            if request.form['Station'] == station_info['id']:
+                return render_template('index2.html', last_station=station_info['last'], next_station=station_info['next'],
+                                       arrive1=station_info['arrive1'], arrive2=station_info['arrive2'],
+                                       progress_value_platform=progress_value_platform,
+                                       progress_value_inside=progress_value_inside)
+    else:
+        return render_template('index2.html', progress_value_platform=progress_value_platform,
+                               progress_value_inside=progress_value_inside)
+
+
+'''@app.route("/index2", methods =['GET','POST'])
+def index2(): #여기에 특정 값을 저장한 변수를 넣기
+    progress_value_platform = random.randrange(0,100)
+    progress_value_inside = random.randrange(0,100)
+    if request.method =='POST':
+        for station_info in topics:
+            if request.form['Station'] == station_info['id']:
+                return render_template('index2.html', last_station = station_info['last'], next_station = station_info['next'], arrive1 = station_info['arrive1'], arrive2 = station_info['arrive2'],progress_value_platform = progress_value_platform, progress_value_inside = progress_value_inside)
+    else:
+        return render_template('index2.html',progress_value_platform = progress_value_platform, progress_value_inside = progress_value_inside)
+'''
+
+# @app.route("/index2/1/", methods=['GET','POST'])
+# def index2_1():
+#     progress_value_platform = random.randrange(0,100)
+#     progress_value_inside = random.randrange(0,100)
+#     for station_info in topics:
+#         if station_info['id'] == '종각역':
+#             return render_template('index2.html', last_station = station_info['last'], next_station = station_info['next'], arrive1 = station_info['arrive1'], arrive2 = station_info['arrive2'],progress_value_platform = progress_value_platform, progress_value_inside = progress_value_inside)
+
+
+@app.route("/data")
+def data():
+    return render_template('data.html')
+
+@app.route("/시청_station_daily_data")
+def si_station_daily_data():
+    return render_template('시청_station_daily_data.html')
+
+@app.route("/종각_station_daily_data")
+def jong_station_daily_data():
+    return render_template('종각_station_daily_data.html')    
+    
+@app.route("/서울_station_daily_data")
+def se_station_daily_data():
+    return render_template('서울_station_daily_data.html')
+
+@app.route('/video')
+def video():
+    return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__=="__main__":
+    app.run()
+
+
+# 지도는 request로 불러와야함
